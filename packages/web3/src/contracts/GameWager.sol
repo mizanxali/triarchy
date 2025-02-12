89818705de51c7ca67a8532122d3c50127999d23
mizanxali <mizanalip786@gmail.com>
2025-02-12 21:56:11 +0530

    add withdraw function for owner

--- packages/web3/src/contracts/GameWager.sol
+++ packages/web3/src/contracts/GameWager.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 
 contract GameWager {
     address public owner;
-    
+
     constructor() {
         owner = msg.sender;
     }
-    
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
         _;
@@ -25,29 +25,35 @@ contract GameWager {
 
     mapping(string => Game) public games;
     string[] public allGameCodes;
-    
+
     event GameCreated(string gameCode, address player1, uint256 wagerAmount);
     event PlayerJoined(string gameCode, address player2);
     event GameComplete(string gameCode, address winner, uint256 prizeMoney);
     event GameCanceled(string gameCode);
-    
+    event WithdrawComplete(address owner, uint256 amount);
+
     modifier gameExists(string memory gameCode) {
         require(games[gameCode].player1 != address(0), "Game does not exist");
         _;
     }
 
-    function getGame(string memory gameCode) external view returns (Game memory) {
+    function getGame(
+        string memory gameCode
+    ) external view returns (Game memory) {
         return games[gameCode];
     }
-    
+
     function getAllGameCodes() external view returns (string[] memory) {
         return allGameCodes;
     }
 
     function createGame(string memory gameCode) external payable {
         require(msg.value > 0, "Wager amount must be greater than 0");
-        require(games[gameCode].player1 == address(0), "Game code already exists");
-        
+        require(
+            games[gameCode].player1 == address(0),
+            "Game code already exists"
+        );
+
         games[gameCode] = Game({
             gameCode: gameCode,
             player1: msg.sender,
@@ -57,28 +63,36 @@ contract GameWager {
             isComplete: false,
             winner: address(0)
         });
-        
+
         allGameCodes.push(gameCode);
-        
+
         emit GameCreated(gameCode, msg.sender, msg.value);
     }
-    
-    function joinGame(string memory gameCode) external payable gameExists(gameCode) {
+
+    function joinGame(
+        string memory gameCode
+    ) external payable gameExists(gameCode) {
         Game storage game = games[gameCode];
-        
+
         require(game.isActive, "Game is not active");
         require(game.player2 == address(0), "Game already has two players");
         require(msg.sender != game.player1, "Cannot join your own game");
-        require(msg.value == game.wagerAmount, "Must match the exact wager amount");
-        
+        require(
+            msg.value == game.wagerAmount,
+            "Must match the exact wager amount"
+        );
+
         game.player2 = msg.sender;
-        
+
         emit PlayerJoined(gameCode, msg.sender);
     }
-    
-    function completeGame(string memory gameCode, address winner) external onlyOwner gameExists(gameCode) {
+
+    function completeGame(
+        string memory gameCode,
+        address winner
+    ) external onlyOwner gameExists(gameCode) {
         Game storage game = games[gameCode];
-        
+
         require(game.isActive, "Game is not active");
         require(!game.isComplete, "Game is already complete");
         require(game.player2 != address(0), "Game needs two players");
@@ -86,30 +100,44 @@ contract GameWager {
             winner == game.player1 || winner == game.player2,
             "Winner must be one of the players"
         );
-        
+
         game.isActive = false;
         game.isComplete = true;
         game.winner = winner;
-        
-        uint256 prizeMoney = game.wagerAmount * 2;
+
+        uint256 totalWager = game.wagerAmount * 2;
+        uint256 prizeMoney = (totalWager * 80) / 100;
         payable(winner).transfer(prizeMoney);
-        
+
         emit GameComplete(gameCode, winner, prizeMoney);
     }
-    
-    function cancelGame(string memory gameCode) external onlyOwner gameExists(gameCode) {
+
+    function cancelGame(
+        string memory gameCode,
+        address compensatedPlayer
+    ) external onlyOwner gameExists(gameCode) {
         Game storage game = games[gameCode];
         require(game.isActive, "Game is not active");
-        
+        require(
+            compensatedPlayer == game.player1 ||
+                compensatedPlayer == game.player2,
+            "Compensated player must be one of the players"
+        );
+
         game.isActive = false;
         game.isComplete = true;
-        
-        payable(game.player1).transfer(game.wagerAmount);
-        
-        if (game.player2 != address(0)) {
-            payable(game.player2).transfer(game.wagerAmount);
-        }
+
+        payable(compensatedPlayer).transfer(game.wagerAmount);
 
         emit GameCanceled(gameCode);
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+
+        payable(owner).transfer(balance);
+
+        emit WithdrawComplete(owner, balance);
     }
 }
